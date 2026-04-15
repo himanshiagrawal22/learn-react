87f62b055dea737c4225660fae7f79953c3f6ef8
himanshiagrawal22 <himanshiagrawal222@gmail.com>
2026-04-15 16:49:46 +0530

    Added Tailwind setup and updated UI

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold bg-red-500 p-6 rounded-2xl">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default App;
+
